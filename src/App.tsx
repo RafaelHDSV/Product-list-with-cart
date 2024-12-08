42981d3a28ac77b380686f590638af897dc1bd45
Rafael Vieira <rafaelvieira1720@gmail.com>
2024-12-08 14:49:45 -0300

    refactor: update images route

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Product from './components/Product/Product'
+import data from './data/data.json'
+import styles from './App.module.scss'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [cart, setCart] = useState<{ name: string; quantity: number }[]>([])
+
+  const handleAddToCart = (productName: string) => {
+    setCart(prevCart => {
+      const existingProductIndex = prevCart.findIndex(item => item.name === productName)
+      if (existingProductIndex !== -1) {
+        const updatedCart = [...prevCart]
+        updatedCart[existingProductIndex].quantity += 1
+        return updatedCart
+      } else {
+        return [...prevCart, { name: productName, quantity: 1 }]
+      }
+    })
+  }
+
+  console.log(cart)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.app}>
+      <div className={styles.main}>
+        <div className={styles.productContainer}>
+          <h1>Desserts</h1>
+
+          <div className={styles.productList}>
+            {data.map(product => {
+              return (
+                <Product key={product.name} data={product} cart={cart.filter(productCart => productCart.name === product.name)} onAddToCart={handleAddToCart} />
+              )
+            })}
+          </div>
+        </div>
+
+        <div className={styles.cart}>
+          <h2>{`Your Cart (${cart.length})`}</h2>
+
+          {!cart.length && (
+            <div className={styles.cartEmpty}>
+              <img src='images/illustration-empty-cart.svg' alt='illustration-empty-cart.svg' />
+              <span>Your added items will appear here</span>
+            </div>
+          )}
+
+          {cart.map((product, index) => {
+            return <span key={index}>{`${index} - ${product.name} (Quantity: ${product.quantity})`}</span>
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {/* <div className={styles.attribution}>
+        Challenge by <a href='https://www.frontendmentor.io?ref=challenge'>Frontend Mentor</a>. Coded by <a href='#'>Your Name Here</a>.
+      </div> */}
+    </div>
   )
 }
-
-export default App
