814b497f423154690a2f5c7397016f71e671b80a
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-02-08 23:10:56 -0300

    feat: add DecrementIcon and IncrementIcon components for quantity adjustment

---
+++ src/assets/icons/DecrementIcon.tsx
@@ -0,0 +1,11 @@
+export function DecrementIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      viewBox='0 0 10 2'
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path fill='currentColor' d='M0 .375h10v1.25H0V.375Z' />
+    </svg>
+  )
+}
