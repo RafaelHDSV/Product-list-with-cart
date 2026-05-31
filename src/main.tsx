9570e60c84dc2fb5047ce83e89a612fe54e162b9
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-31 03:38:12 -0300

    feat: add Vieira Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { VieiraAnalytics } from '@vieira/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <VieiraAnalytics projectKey='product-list-with-cart' />
   </StrictMode>,
 )
